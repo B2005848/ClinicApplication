186f01964ef3a0e8631d482e1be4fb56cbe17dcf
B2005848 <namltgt2002@gmail.com>
2024-10-22 01:03:36 +0700

    upload screen booking

---
+++ src/screens/BookingScreenNew/style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Open Sans-Medium",
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  bgrButton: {
+    height: 40,
+    width: 250,
+    backgroundColor: "#48bbc6",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  titleButton: {
+    fontSize: 18,
+    color: "#fff",
+    fontFamily: "Open Sans-Bold",
+  },
+});
+
+export default styles;
